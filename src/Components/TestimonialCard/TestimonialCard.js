e0642f7d9b13a982ed1c7a9aace1927f72ec8659
Anushka Agrawal <agrawalanushka512@gmail.com>
2023-04-30 17:41:02 +0530

    Update TestimonialCard.js

--- src/Components/TestimonialCard/TestimonialCard.js
+++ src/Components/TestimonialCard/TestimonialCard.js
@@ -6,10 +6,9 @@ import "./TestimonialCard.scss";
 function TestimonialCard({ name, desc, imgUrl }) {
   return (
     <article className="testimonial__card flex__center">
-      <img className="userIMg" src={imgUrl} alt="user" />
-      <h4>{name}</h4>
+      <h4 className="name">{name}</h4>
       <p className="p-text">{desc}</p>
-      <img src={Stars} alt="RatingStart" />
+      <img src={Stars} alt="RatingStart" className="stars"/>
     </article>
   );
 }
